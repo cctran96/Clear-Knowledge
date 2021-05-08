773f01f7db56f41cbebea143b3b6496a2f95c9eb
Chau Tran <cctran96@gmail.com>
2021-05-08 00:03:11 -0700

    added routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import AboutUs from './components/AboutUs'
+import Favorites from './components/Favorites'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <Router>
+        <div id='layout'>
+          <Navbar />
+          <Route exact path="/" component={Home}/>
+          <Route path="/aboutus" component={AboutUs}/>
+          <Route path="/favorites" component={Favorites}/>
+        </div>
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
