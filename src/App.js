12eb896d8436ddf22d0530417c8d2216513faea0
Chau Tran <cctran96@gmail.com>
2021-05-08 19:59:05 -0700

    added about us details and css

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import AboutUs from './components/AboutUs'
-import Favorites from './components/Favorites'
+import Profile from './components/Profile'
+import Search from './components/Search'
 
 class App extends React.Component {
   render(){
     return (
       <Router>
-        <div id='layout'>
-          <Navbar />
-          <Route exact path="/" component={Home}/>
-          <Route path="/aboutus" component={AboutUs}/>
-          <Route path="/favorites" component={Favorites}/>
+        <div className='container'>
+            <Navbar />
+            <Route exact path="/" component={Home}/>
+            <Route path="/aboutus" component={AboutUs}/>
+            <Route path="search" component={Search}/>
+            <Route path="/profile" component={Profile}/>
         </div>
       </Router>
     )
