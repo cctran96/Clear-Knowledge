cc9734cf72c0e722025a5f687f1bfea0c136e73b
Chau Tran <cctran96@gmail.com>
2021-05-10 14:31:26 -0700

    fixed key error

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-const BookCard = ({book}) => 
-    {
+const BookCard = ({book}) => {
         const img = book.cover_i ?
         `http://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg` :
         'https://image.shutterstock.com/image-vector/no-user-profile-picture-hand-260nw-99335579.jpg'
         return (
-            <div key={book.key} className='card'>
+            <div className='card'>
                 <div className="card-body">
                     <div className="card-front">
                         <img src={img} alt={book.title}/>
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,14 +2,10 @@ import React from 'react'
 import BookCard from './BookCard'
 
 const SearchResults = ({books}) => {
-    const card = books ? books.map(book => 
-        {
-        return (
-            <div>
-                <BookCard book = {book}/>
-            </div>
-        )
+    const card = books ? books.map(book => {
+    return <BookCard key={book.key} book={book}/>
     }) : null
+    
     return(
         <div className='cards'>
             {card}
