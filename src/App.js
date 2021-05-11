1e4347ad95ca4c9380922da5ff8094d58105130b
Chau Tran <cctran96@gmail.com>
2021-05-10 17:31:24 -0700

    login and create account functionality added

--- src/App.js
+++ src/App.js
@@ -7,16 +7,61 @@ import AboutUs from './containers/AboutUs'
 import Profile from './containers/Profile'
 import Search from './containers/Search'
 
+const usersURL = 'http://localhost:3001/users/'
+const fetchUsers = () => fetch(usersURL).then(r => r.json())
+
 class App extends React.Component {
+  state={
+    users: [],
+    currentUser: ''
+  }
+
+  componentDidMount(){
+    fetchUsers().then(users => this.setState({users: users.map(user => user.username)}))
+  }
+
+  handleLogin = (e, u, p) => {
+    e.preventDefault()
+    fetchUsers().then(data => {
+        const passed = data.find(user => user.username === u && user.password === p)
+        passed ? this.setState({currentUser: u, username: '', password: ''}) : alert("The information you have entered is incorrect.")
+    })
+  } 
+
+  handleLogout = () => {
+    this.setState({
+      currentUser: ''
+    })
+  }
+
+  createAccount = (e, u, p) => {
+    e.preventDefault()
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({username: u, password: p})
+    }
+    this.state.users.includes(u) ? alert('There is already someone with that username!') 
+    :fetch(usersURL, config).then(r => r.json()).then(data => {
+      this.setState(
+        {currentUser: data.username, 
+          users: [...this.state.users, data.username]
+        })
+      })
+  }
+
   render(){
     return (
       <Router>
         <div className='container'>
             <Navbar />
-            <Route exact path="/" component={Home}/>
+            <Route exact path="/" component={() => <Home handleLogin={this.handleLogin} handleLogout={this.handleLogout} createAccount={this.createAccount} currentUser={this.state.currentUser}/>}/>
             <Route path="/aboutus" component={AboutUs}/>
             <Route path="/search" component={Search}/>
-            <Route path="/profile" component={Profile}/>
+            <Route path="/profile" component={() => <Profile currentUser={this.state.currentUser}/>}/>
         </div>
       </Router>
     )
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,66 @@
 import React, { Component } from 'react'
 
 class Home extends Component {
+    state={
+        username: '',
+        password: '',
+        newUser: '',
+        newPass: ''
+    }
+
+    handleChange = e => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    handleClick = e => {
+        const login = e.target.parentNode.lastChild
+        login.classList.toggle('show')
+    }
+
+    showAccount = () => {
+        const form = document.querySelector('.create')
+        form.classList.toggle('show')
+    }
+
+    newInfo = e => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
     render(){
         return(
             <div className='page-title'>
-                <h2>Title</h2>
-                <p>Something</p>
+                {this.props.currentUser ? 
+                <div>
+                    <p>You are signed in as <i>{this.props.currentUser}</i></p>
+                    <button onClick={this.props.handleLogout}>Logout</button>
+                </div>
+                :
+                <div>
+                    <button onClick={this.handleClick}>Login</button>
+                    <button onClick={this.showAccount}>Create an account</button>
+                    <div className='create'>
+                        <form onSubmit={e => this.props.createAccount(e, this.state.newUser, this.state.newPass)}>
+                            <label>Enter a username: </label>
+                            <input type="text" name="newUser" value={this.state.newUser} onChange={this.newInfo}/>
+                            <label>Create your password: </label>
+                            <input type="password" name="newPass" value={this.state.newPass} onChange={this.newInfo}/>
+                            <input className='submit-btn' type='submit' />
+                        </form>
+                    </div>
+                    <div className='login'>
+                        <form onSubmit={e => this.props.handleLogin(e, this.state.username, this.state.password)}>
+                            <label>Username: </label>
+                            <input type="text" name="username" value={this.state.username} onChange={this.handleChange}/>
+                            <label>Password: </label>
+                            <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+                            <input className='submit-btn' type='submit' />
+                        </form>
+                    </div>
+                </div>}
             </div>
         )
     }
