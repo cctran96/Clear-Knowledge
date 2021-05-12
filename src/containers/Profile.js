d0d9587fbaeff6af9d87198af732ca189faa5ddc
DanteSeattle <gabrieldantelombardi@gmail.com>
2021-05-12 10:38:10 -0700

    favorites added

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
+import BookCard from "../components/BookCard"
 
 class Profile extends Component {
+
+    filterBooks = (book) => {
+        if(this.props.currentUser.username){
+            if (book.currentUser.username === this.props.currentUser.username) {
+                return <BookCard key={book.book.id} book={book.book} currentUser={this.props.currentUser} favoriteBook = {this.props.favoriteBook}/>
+            }
+        }
+      }
+
     render(){
         const user = this.props.currentUser
         return(
@@ -15,11 +25,13 @@ class Profile extends Component {
                         <p>{user.bio}</p>
                         <button>Edit Profile</button>
                     </div>
+                    {this.props.favorites.map(book => this.filterBooks(book))}
                 </div> :
                 <div>
                     <h2>Log in to access your profile</h2>
                 </div>
                 }
+                
             </div>
         )
     }
